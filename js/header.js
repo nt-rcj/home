a14950dc49dde120b5edfb20bd269db1f1db8a4c
nt-rcj <73057436+nt-rcj@users.noreply.github.com>
2020-11-02 23:40:47 +0900

    Add files via upload

---
+++ js/header.js
@@ -0,0 +1,24 @@
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
+$(".hover").mouseleave(
+    function () {
+      $(this).removeClass("hover");
+    }
+  );
+
+
+  window.addEventListener( "scroll", function() {
+
+    var headerElement = document.getElementById( "header" ) ; // `#header`セレクタを取得
+    var rect = headerElement.getBoundingClientRect() ; // 
+    var y = rect.top + window.pageYOffset ; // Y方向 (縦)にスクロール量を取得（pageYOffset：windowオブジェクト。現在表示位置のY座標を取得）
+    if (y > 0) { // 変数yの値が0よりも
+      headerElement.classList.remove('hidden'); // 大きければhiddensセレクタを削除する
+    } else {
+      headerElement.classList.add('hidden'); // そうでなければhiddensセレクタを追加する
+    }
+  } ) ;  
