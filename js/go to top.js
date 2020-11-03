395cc77477b243da0a83bfe77aed7ebe083b09c2
nt-rcj <73057436+nt-rcj@users.noreply.github.com>
2020-11-03 18:40:45 +0900

    js

--- js/go to top.js
+++ js/go to top.js
@@ -1,15 +1,18 @@
-$('#pagetop').hide();
- $(window).scroll(function () {
-  if ($(this).scrollTop() > 300) {
-   $('#pagetop').fadeIn();
-  } else {
-   $('#pagetop').fadeOut();
-  }
-});
-
-$('#pagetop').click(function () {
- $('body,html').animate({
-  scrollTop: 0
- }, 700);
- return false;
+window.addEventListener('DOMContentLoaded', function(){
+  /** jQueryの処理 */
+  $('#pagetop').hide();
+  $(window).scroll(function () {
+   if ($(this).scrollTop() > 300) {
+    $('#pagetop').fadeIn();
+   } else {
+    $('#pagetop').fadeOut();
+   }
+ });
+ 
+ $('#pagetop').click(function () {
+  $('body,html').animate({
+   scrollTop: 0
+  }, 700);
+  return false;
+ });  
 });
